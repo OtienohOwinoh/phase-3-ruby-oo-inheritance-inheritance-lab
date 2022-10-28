9427a884625100dc479121f17bb0cc070bbf26d9
OtienohOwinoh <coowinoh@gmail.com>
2022-10-28 08:54:11 +0300

    commit tree 1f107df61a760783e395b47c1040a4bd6b0ccec4

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    @knowledge=[]
+
+    def initialize 
+        @knowledge=[]
+
+    end
+
+    def learn (str)
+        @knowledge<< str
+
+    end
+
+
 
 end
